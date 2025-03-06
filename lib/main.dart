33388c5d011efdaa5a2c74aa06fdc4dcb79c66a0
ANGEL_MISAEL_GARCIA_HERNANDEZ <a.22308051281193@cbtis128.edu.mx>
2025-03-06 19:52:10 +0000

    otra

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false, // Elimina el banner de depuración
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ElevatedButton'), // Agrega un título al AppBar
+        ),
+        body: Column(
+          children: <Widget>[
+            buildListTile(Icons.home, 'GO to home', Colors.purple),
+            buildListTile(Icons.home, 'GO to home', Colors.grey),
+            buildListTile(Icons.home, 'GO to home', Colors.black),
+            buildListTile(Icons.home, 'GO to home', Colors.pink),
+            buildListTile(Icons.settings, '', Colors.blue),
+            buildListTile(Icons.person, '', Colors.green),
+            buildListTile(Icons.notifications, '', Colors.orange),
+            buildListTile(Icons.favorite, '', Colors.red),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      // Acción para el botón de texto
+                    },
+                    child: Text('TextButton'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Acción para el botón elevado
+                    },
+                    child: Text('Elevated'),
+                  ),
+                ],
+              ),
+            ),
+            // Agrega el Text con tu nombre y matrícula
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Angel Garcia Mat:22308051281193",
+                style: TextStyle(
+                  color: Color(0xb3050505),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget buildListTile(IconData icon, String text, Color color) {
+    return ListTile(
+      leading: Container(
+        alignment: Alignment.center,
+        width: 40, // Ajusta el ancho según sea necesario
+        child: Icon(icon, color: color),
+      ),
+      title: Text(text),
+      horizontalTitleGap: 0,
+      contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+      visualDensity: VisualDensity.compact,
+      minLeadingWidth: 0,
     );
   }
 }
